935253247802da555f55eb650594f693bf98c36d
Kobayashi, L Mitiko <105651010+Mitiko20@users.noreply.github.com>
2023-12-05 22:12:03 -0300

    Updates

---
+++ precache-manifest.4cace02cf842433e8cd79ed0efacdf13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e780bf6cdcd609e4e69158f105484d57",
+    "url": "/DW2A4_Adote_front/index.html"
+  },
+  {
+    "revision": "b372f3d7b0ca5303e88f",
+    "url": "/DW2A4_Adote_front/static/css/main.103d5b26.chunk.css"
+  },
+  {
+    "revision": "0ae67d120f9d08182031",
+    "url": "/DW2A4_Adote_front/static/js/2.7ef452dd.chunk.js"
+  },
+  {
+    "revision": "e8658e50dfaa215d03e17c686961427f",
+    "url": "/DW2A4_Adote_front/static/js/2.7ef452dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b372f3d7b0ca5303e88f",
+    "url": "/DW2A4_Adote_front/static/js/main.acbf01a2.chunk.js"
+  },
+  {
+    "revision": "f444cd827e0370bbd3cc",
+    "url": "/DW2A4_Adote_front/static/js/runtime-main.44a07d44.js"
+  },
+  {
+    "revision": "aa3c94514d02d0d42c4e3fe1f05bad3c",
+    "url": "/DW2A4_Adote_front/static/media/logo.aa3c9451.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/DW2A4_Adote_front/precache-manifest.4cace02cf842433e8cd79ed0efacdf13.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DW2A4_Adote_front/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
